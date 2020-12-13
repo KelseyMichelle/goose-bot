aad3b7ddb764ecb2071b5f3ef31f63858cc9c917
kelsey <kgregware@umass.edu>
2020-12-13 15:59:44 -0500

    added archive function and generalized the logger function

--- config/filepaths.js
+++ config/filepaths.js
@@ -1,7 +1,11 @@
 const memefolder = "./memes/";
 const memelog = `${memefolder}memes.json`;
+const archiveFolder = "./archive/";
+const archiveLog = `${archiveFolder}archive.json`
 
 module.exports = {
     memeFolder: memefolder,
-    memeLog: memelog
+    memeLog: memelog,
+    archiveFolder: archiveFolder,
+    archiveLog: archiveLog
 }
