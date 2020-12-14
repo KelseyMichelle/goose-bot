216aea71a6c1f40a06981a3c15bd7b1056129c2b
kelsey <kgregware@umass.edu>
2020-12-14 10:51:15 -0500

    added filepaths.js

---
+++ config/filepaths.js
@@ -0,0 +1,12 @@
+const memefolder = './memes/';
+const memelog = `${memefolder}memes.json`;
+const archiveFolder = './archive/';
+const archiveLog = `${archiveFolder}archive.json`;
+
+module.exports = {
+  memeFolder: memefolder,
+  memeLog: memelog,
+  archiveFolder: archiveFolder,
+
+  archiveLog: archiveLog,
+};
